f218a2dc525ac9365e3433e399794f1ea57d6128
Sidney Nimako <snibo13@gmail.com>
2023-04-07 14:54:43 -0400

    Adjusted axis

--- src/transform_broadcaster.cpp
+++ src/transform_broadcaster.cpp
@@ -4,8 +4,8 @@
 int main(int argc, char** argv)
 {
 
-    float offsetX = 0;    // Middle
-    float offsetY = 0.23; // Front face
+    float offsetX = 0.23;    // Middle
+    float offsetY = 0; // Front face
     float offsetZ = 0.43; // 17 inches above base
 
     ros::init(argc, argv, "robot_tf_publisher");
@@ -22,10 +22,6 @@ int main(int argc, char** argv)
                 tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(offsetX, offsetY, offsetZ)),
                 ros::Time::now(), "base_link", "base_lidar"));
 
-        broadcaster.sendTransform(
-            tf::StampedTransform(
-                tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0, 0, 0)),
-                ros::Time::now(), "map","base_link"));
 
         r.sleep();
     }
